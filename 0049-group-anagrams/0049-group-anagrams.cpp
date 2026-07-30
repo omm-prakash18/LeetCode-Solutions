ff191329bfc45c008ed1f8219b7bfd879f125c64
Omm Prakash <prakashomm125@gmail.com>
2026-07-30 20:12:43 +0530

    Time: 17 ms (53.06%), Space: 25.1 MB (68.45%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> anagramGroups;
+        
+        for (string s : strs) {
+            string key = s; 
+            
+            sort(key.begin(), key.end()); 
+            
+            anagramGroups[key].push_back(s); 
+        }
+        
+        vector<vector<string>> result;
+        for (auto& pair : anagramGroups) {
+            result.push_back(pair.second);
+        }
+        
+        return result;
+    }
+};
