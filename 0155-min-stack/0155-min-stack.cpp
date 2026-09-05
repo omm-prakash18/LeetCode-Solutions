9f50f3103f49728c6a70fbc0ebc4273023824797
Omm Prakash <prakashomm125@gmail.com>
2026-09-05 23:58:24 +0530

    Time: 57 ms (61.01%), Space: 151 MB (93.2%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,23 +1,20 @@
-#include <stack>
-#include <utility>   // For std::pair
-#include <algorithm> // For std::min
 
 class MinStack {
 private:
-    // Stack stores pairs: {value, current_minimum}
+    
     std::stack<std::pair<int, int>> st;
 
 public:
-    // Constructor
+    
     MinStack() {
     }
     
     void push(int val) {
         if (st.empty()) {
-            // If empty, the first value is also the minimum
+            
             st.push({val, val});
         } else {
-            // Calculate new minimum and push the pair
+            
             int current_min = st.top().second;
             st.push({val, std::min(val, current_min)});
         }
@@ -30,12 +27,12 @@ public:
     }
     
     int top() {
-        // .first gives access to the actual value
+        
         return st.top().first;
     }
     
     int getMin() {
-        // .second gives access to the stored minimum
+        
         return st.top().second;
     }
 };
