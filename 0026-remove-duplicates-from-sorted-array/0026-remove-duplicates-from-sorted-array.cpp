1a69dbccb5df73ebda94be77bcffec43a9151905
Omm Prakash <prakashomm125@gmail.com>
2026-09-12 09:28:20 +0530

    Time: 0 ms (100%), Space: 22.7 MB (18.54%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -2,14 +2,12 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) 
     {
-        // 1. Handle the edge case of an empty array
         if (nums.empty()) return 0;
 
-        // 2. Use .size() instead of .length()
         int n = nums.size();
         int j = 1;
 
-        // 3. Start i at 1 since the first element is always unique
+      
         for(int i = 1; i < n; i++)
         {
             if (nums[i] != nums[j-1])
